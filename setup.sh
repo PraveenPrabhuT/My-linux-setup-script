74f3493f13891273904646f020a55c2b4374ee40
Tanmay Paturu <tintedfireglass@hotmail.com>
2022-11-03 05:55:19 +0000

    Fixed Directory issue

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@ su
 pacman -Syu
 pacman -S -y sublime-text4 chromium blender htop neofetch keepassxc vim
 wget https://mega.nz/linux/repo/Arch_Extra/x86_64/megasync-4.7.1-3-x86_64.pkg.tar.zst
-cd Downloads
 pacman -U megasync-4.7.1-3-x86_64.pkg.tar.zst
 reboot
